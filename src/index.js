19d0a55d3af6e77b2845243232d1ad4b9ada7d95
Bonnie136 <bonnie.wells@gmail.com>
2026-05-03 16:10:52 +0100

    Added basic form submit Javascript functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateHaiku(event) {
+  event.preventDefault();
 
+  new Typewriter("#haiku", {
+    strings: "haiku here",
+    autoStart: true,
+    cursor: "",
+    delay: 40,
+  });
+}
+
+let haikuGeneratorForm = document.querySelector("#haiku-form-submit");
+haikuGeneratorForm.addEventListener("submit", generateHaiku);
